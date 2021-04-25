43dd094f2e95932b7d10eb031acba1b362726635
MishailAJ <74947150+MishailAJ@users.noreply.github.com>
2021-04-25 14:50:55 +0700

    Delete program.py

--- program.py
+++
@@ -1,9 +0,0 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
-
-
-if __name__ == '__main__':
-    main()
